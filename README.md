cf6fafdbd0ddf92f33fd29c06a886324647242dd
Brian <legobmw99@gmail.com>
2019-09-08 12:17:49 -0400

    RightClickItem is fired serverside (fix #8)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-BetterThanMending 1.3.1
+BetterThanMending 1.3.2
 =========
 [![ CurseForge](http://cf.way2muchnoise.eu/264738.svg)](https://minecraft.curseforge.com/projects/better-than-mending)
 
 A simple mod to improve Mending, allowing you to repair items by shift-right clicking and draining experience
 
-This mod is currently updated for `Minecraft 1.14.4` and `Forge 28.0.11`
+This mod is currently updated for `Minecraft 1.14.4` and `Forge 28.0.100`
 
 Please verify and report any issues!
--- src/main/java/com/legobmw99/BetterThanMending/BetterThanMending.java
+++ src/main/java/com/legobmw99/BetterThanMending/BetterThanMending.java
@@ -1,30 +1,21 @@
 package com.legobmw99.BetterThanMending;
 
-import com.legobmw99.BetterThanMending.network.packets.RepairItemPacket;
+import com.legobmw99.BetterThanMending.util.Utilities;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.NetworkRegistry;
-import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 
 @Mod(BetterThanMending.MODID)
 public class BetterThanMending {
     public static final String MODID = "betterthanmending";
-
-    public static final SimpleChannel NETWORK = NetworkRegistry.ChannelBuilder.named(new ResourceLocation(MODID, "networking"))
-            .clientAcceptedVersions(s -> true)
-            .serverAcceptedVersions(s -> true)
-            .networkProtocolVersion(() -> "1.0.0")
-            .simpleChannel();
 
 
     public BetterThanMending() {
@@ -33,18 +24,21 @@ public class BetterThanMending {
 
     public void init(final FMLCommonSetupEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
-        NETWORK.registerMessage(0, RepairItemPacket.class, RepairItemPacket::write, RepairItemPacket::read, RepairItemPacket::onMessage);
     }
 
 
     @SubscribeEvent
     public void onItemUse(final PlayerInteractEvent.RightClickItem event) {
-        PlayerEntity playerEntity = event.getEntityPlayer();
+        PlayerEntity player = event.getPlayer();
         ItemStack stack = event.getItemStack();
-        if (stack.isDamaged() && EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0) {
-            if (playerEntity.isSneaking() && ((playerEntity.experienceTotal >= 2) || (playerEntity.experienceLevel > 0))) {
-                NETWORK.sendToServer(new RepairItemPacket());
-                event.setCanceled(true);
+
+        if (!player.world.isRemote()) {
+            if (stack.isDamaged() && EnchantmentHelper.getEnchantmentLevel(Enchantments.MENDING, stack) > 0) {
+                if (player.isSneaking() && ((player.experienceTotal >= 2) || (player.experienceLevel > 0))) {
+                    stack.setDamage(stack.getDamage() - 4);
+                    Utilities.addPlayerXP(player, -2);
+                    event.setCanceled(true);
+                }
             }
         }
     }
