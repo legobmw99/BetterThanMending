af18d425df6d7d70d51462b943ac871f203bc500
Brian <legobmw99@gmail.com>
2024-01-16 21:30:07 -0500

    Fabric: Register callback with identifier

--- fabric/src/main/java/com/legobmw99/BetterThanMending/BetterThanMending.java
+++ fabric/src/main/java/com/legobmw99/BetterThanMending/BetterThanMending.java
@@ -2,6 +2,7 @@ package com.legobmw99.BetterThanMending;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.ItemStack;
 
@@ -9,7 +10,7 @@ public class BetterThanMending implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        UseItemCallback.EVENT.register((playerEntity, world, hand) -> {
+        UseItemCallback.EVENT.register(new ResourceLocation(Common.MODID, "try_to_mend"), (playerEntity, world, hand) -> {
             ItemStack stack = playerEntity.getItemInHand(hand);
 
             if (Common.onItemUse(playerEntity, stack, 2.0f)) {
